d63cd55197c93d990b4a466b5d0373230d340449
Juan Velarde <velardejuanm@gmail.com@>
2022-12-15 20:12:56 +0100

    vezba2

---
+++ def/vezba2.py
@@ -0,0 +1,11 @@
+class Team:
+    def __init__(self, naziv_tima):
+        self.naziv_tima = naziv_tima
+
+class Score:
+    domaci = 0
+    gosti = 0
+
+class March:    #       Team Team   Int     Score             
+    def __init__(self, tim1, tim2, minuti, score):                           #def je inicijator ili konstruktor  
+        
